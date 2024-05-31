2bef7c318ff7e9be64a392f9460c729d1c87913b
thirukumaran05 <learningthiru@gmail.com>
2024-05-31 19:43:21 +0530

    Added description

--- components/SEO.js
+++ components/SEO.js
@@ -4,8 +4,12 @@ export default function SEO({ title, description }) {
   return (
     <Head>
       <title>{title}</title>
-      <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <meta
+        name="description"
+        content="Discover the ultimate API for riddles! Access a vast collection of brain-teasers, puzzles, math, mystery, funny, who am i and logical riddles to enhance your apps and websites. Perfect for developers, educators, and riddle enthusiasts. Try our Riddles API today and challenge your mind!"
+      />
+      <meta name="keywords" content="riddles-api, api, riddles" />
     </Head>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import SEO from '../components/SEO';
 export default function Index({ posts, globalData }) {
   return (
     <Layout>
-      <SEO title="Riddles API" description={globalData.blogTitle} />
+      <SEO title="Riddles API" />
       <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
